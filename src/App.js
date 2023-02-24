d5ff442e22927a120a89cac1ce125a64b2f5d0e9
Sasha K <nortsandra@gmail.com>
2023-02-24 10:23:38 +0200

    improved boostrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./Header";
 import Forecast from "./Forecast";
 import Days from "./Days";
 import Footer from "./Footer";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
