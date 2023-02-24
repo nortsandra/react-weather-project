b9ece3607690ed121841432898a3892978a658e1
Sasha K <nortsandra@gmail.com>
2023-02-24 16:15:08 +0200

    updated css and html

--- src/Footer.js
+++ src/Footer.js
@@ -8,9 +8,16 @@ export default function Footer() {
           href="https://github.com/nortsandra/react-weather-project"
           target="_blank"
           rel="noreferrer">
-          Open-source code
+          Open-source code {""}
         </a>
-        by Oleksandra Kolomiiets for SheCodes React
+        by{" "}
+        <a
+          href="https://www.linkedin.com/in/oleksandra-kolomiiets-74272022b/"
+          target="blank"
+          rel="noreferrer">
+          Oleksandra Kolomiiets{" "}
+        </a>
+        for SheCodes React
       </p>
     </div>
   );
